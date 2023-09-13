5ece9282c3cd77db819fcc358b8a73cc99c2f3f2
lihyel <0907leeharry@gmail.com>
2023-09-13 17:33:26 +0900

    첫 번째 커밋

---
+++ boards/common/common.js
@@ -0,0 +1,1 @@
+const backEndDomain = "http://localhost:3000";
---
+++ students/common/common.js
@@ -0,0 +1,4 @@
+const backEndDomain = "http://localhost:3020";
+
+// 3000 포트는 boards 전용으로 쓰기로
+//const backEndDomain = "http://localhost:3000";
